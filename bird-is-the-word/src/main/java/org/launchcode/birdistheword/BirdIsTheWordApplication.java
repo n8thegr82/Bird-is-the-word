9f8a9756a6ec1c60a07dc851216952fcb8256ffd
mollyozzie <78570465+mollyozzie@users.noreply.github.com>
2021-08-16 18:11:14 -0500

    Add files via upload

---
+++ bird-is-the-word/src/main/java/org/launchcode/birdistheword/BirdIsTheWordApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.birdistheword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdIsTheWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdIsTheWordApplication.class, args);
+	}
+
+}
---
+++ bird-is-the-word/src/test/java/org/launchcode/birdistheword/BirdIsTheWordApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.birdistheword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdIsTheWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
